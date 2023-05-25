b7eadc621a0c8bd20f309c2c3c95cf58681ef205
Lucas Bandeira <lucascoliado@hotmail.com>
2023-05-25 20:33:46 -0300

    Use local folder for training data

---
+++ Naive Bayes/rede_bayesiana.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 24 21:13:07 2023
+
+@author: deivity.andrade
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from yellowbrick.classifier import ConfusionMatrix
+#%% Importando os dados
+csv_path = './treino_bayseana.txt'
+df = pd.read_table(csv_path)
+csv_path = './teste_bayseana.txt'
+dft = pd.read_table(csv_path)
+
+#%% Preparando dados para utilização, efetuando a classificação segundo os parâmetros refeência
+for i in range(len(df)):
+    if df['NIF'].iloc[i] < -26:
+         df['NIF'].iloc[i] = "<_-26"
+    else:
+        df['NIF'].iloc[i] = ">=_-26"
+    
+    if df['VT'].iloc[i] >= 315:
+        df['VT'].iloc[i] = ">=_315"
+    else:
+        df['VT'].iloc[i] = "<_315"
+    
+    if df['RR'].iloc[i] < 30:
+        df['RR'].iloc[i] = "<_30"
+    else:
+        df['RR'].iloc[i] = ">=_30"
+    
+    if df['RSBI'].iloc[i] <= 80:
+        df['RSBI'].iloc[i] = "<=_80"
+    else:
+        df['RSBI'].iloc[i] = ">_80"
+
+for i in range(len(dft)):
+    if dft['NIF'].iloc[i] < -26:
+         dft['NIF'].iloc[i] = "<_-26"
+    else:
+        dft['NIF'].iloc[i] = ">=_-26"
+    
+    if dft['VT'].iloc[i] >= 315:
+        dft['VT'].iloc[i] = ">=_315"
+    else:
+        dft['VT'].iloc[i] = "<_315"
+    
+    if dft['RR'].iloc[i] < 30:
+        dft['RR'].iloc[i] = "<_30"
+    else:
+        dft['RR'].iloc[i] = ">=_30"
+    
+    if dft['RSBI'].iloc[i] <= 80:
+        dft['RSBI'].iloc[i] = "<=_80"
+    else:
+        dft['RSBI'].iloc[i] = ">_80"
+#%% Divisão de previsores e classe
+
+x_dados = df.iloc[:, 0:4].values
+y_dados = df.iloc[:, 4].values
+
+x_dadost = dft.iloc[:, 0:4].values
+y_dadost = dft.iloc[:, 4].values
+
+#%% Transformando atributos categoricos em numericos
+
+label_encoder_NIF = LabelEncoder()
+label_encoder_VT = LabelEncoder()
+label_encoder_RR = LabelEncoder()
+label_encoder_RSBI = LabelEncoder()
+
+x_dados[:,0] = label_encoder_NIF.fit_transform(x_dados[:,0])
+x_dados[:,1] = label_encoder_VT.fit_transform(x_dados[:,1])
+x_dados[:,2] = label_encoder_RR.fit_transform(x_dados[:,2])
+x_dados[:,3] = label_encoder_RSBI.fit_transform(x_dados[:,3])
+
+x_dadost[:,0] = label_encoder_NIF.fit_transform(x_dadost[:,0])
+x_dadost[:,1] = label_encoder_VT.fit_transform(x_dadost[:,1])
+x_dadost[:,2] = label_encoder_RR.fit_transform(x_dadost[:,2])
+x_dadost[:,3] = label_encoder_RSBI.fit_transform(x_dadost[:,3])
+print (x_dados)
+
+#%% utilizando e treinando a nayve bayes
+
+naive_desmame = GaussianNB()
+naive_desmame.fit(x_dados, y_dados)
+
+#%% previsao
+previsao = naive_desmame.predict(x_dadost)
+
+#%% analise dos resultados
+#acuracidade
+accuracy_score(y_dadost, previsao)
+#matrix confusao
+confusion_matrix(y_dadost, previsao)
+#plotando a matrix confusao
+cm = ConfusionMatrix(naive_desmame)
+cm.fit(x_dados, y_dados)
+cm.score(x_dadost, y_dadost)
+
+#outras metricas
+print(classification_report(y_dadost, previsao))
--- Rede Fuzzy/Fuzzy_trinf_alt_regra.py
+++ Rede Fuzzy/Fuzzy_trinf_alt_regra.py
@@ -19,7 +19,6 @@ Ação: desmame ou não, entre 0 e 100?
 - onde > 50 permanece entubado e  <=50 desmame
 
 
-
 - Documentação: https://pythonhosted.org/scikit-fuzzy/overview.html
 '''
 
@@ -30,7 +29,7 @@ import pandas as pd
 from skfuzzy import control as ctrl
 
 #%% Importando os dados
-csv_path = 'C:\\Users\\deivity.andrade\\OneDrive - Companhia de Gás de Santa Catarina - SCGÁS\\Área de Trabalho\\fuzzy\\Respira_DB_Tds_RSBI.txt'
+csv_path = './Respira_DB_Tds.txt'
 df = pd.read_table(csv_path)
 
 #%%
@@ -42,13 +41,10 @@ NIF = ctrl.Antecedent(np.arange(int(df[['NIF']].min()), int(df[['NIF']].max()+1)
 VT = ctrl.Antecedent(np.arange(int(df[['VT']].min()), int(df[['VT']].max()+1), 1), 'VT')
 """#Criando um array com o range de casos de RR obtido dos arquivos de treino e teste"""
 RR = ctrl.Antecedent(np.arange(int(df[['RR']].min()), int(df[['RR']].max()+1), 1), 'RR')
-"""#Criando um array com o range de casos de RSBI obtido dos arquivos de treino e teste"""
-RSBI = ctrl.Antecedent(np.arange(int(df[['RSBI']].min()), int(df[['RSBI']].max()+1), 1), 'RSBI')
 #%%
 print("Universo de NIF: %s" % (NIF.universe))
 print("Universo de VT: %s" % (VT.universe))
 print("Universo de RR: %s" % (RR.universe))
-print("Universo de RSBI: %s" % (RSBI.universe))
 #%%
 """#Consequente"""
 """"#Criando um array com o range de acaso de NIF obtido dos arquivos de treino e teste"""
@@ -60,7 +56,6 @@ desmame.universe
 NIF.automf(number = 2, names = ['Sucesso', 'Fracasso'])
 VT.automf(number = 2, names = ['Sucesso', 'Fracasso'])
 RR.automf(number = 2, names = ['Sucesso', 'Fracasso'])
-RSBI.automf(number = 2, names = ['Sucesso', 'Fracasso'])
 #%%
 #Definindo os valores para os critérios do NIF - utilizando a função triangular
 NIF['Sucesso'] = fuzz.trimf(NIF.universe, [-60,-60,-20])
@@ -79,12 +74,7 @@ RR['Fracasso'] = fuzz.trimf(RR.universe, [30,46,46])
 RR['Sucesso'] = fuzz.trimf(RR.universe, [13,13,35])
 
 RR.view()
-#%%
-#Definindo os valores para os critérios do RSBI - utilizando a função triangular
-RSBI['Fracasso'] = fuzz.trimf(RSBI.universe, [80,266,266])
-RSBI['Sucesso'] = fuzz.trimf(RSBI.universe, [14,14,120])
 
-RSBI.view()
 #%%
 #Definindo os valores para os critérios do Desmame - utilizando a função triangular
 desmame['Desmamar'] = fuzz.trimf(desmame.universe, [0, 0, 100])
@@ -94,36 +84,25 @@ desmame.view()
 #Definindo as regras segundo os antecedes para o consequente
 # Pela regra, entendemos que para 2 antecedentes indicando fracasso, o desmame não será efetuado (permanece)
 # Em caso de 2 antecedentes indicando sucesso, o desmame será efetuado (demamar)
-regra1 = ctrl.Rule(NIF['Fracasso'] & VT['Fracasso'] & RR['Fracasso'] & RSBI['Fracasso'], desmame['Permanece'])
-regra2 = ctrl.Rule(NIF['Fracasso'] & VT['Fracasso'] & RR['Fracasso'] & RSBI['Sucesso'], desmame['Permanece'])
-regra3 = ctrl.Rule(NIF['Fracasso'] & VT['Fracasso'] & RR['Sucesso'] & RSBI['Fracasso'], desmame['Permanece'])
-regra4 = ctrl.Rule(NIF['Fracasso'] & VT['Sucesso'] & RR['Fracasso'] & RSBI['Fracasso'], desmame['Permanece'])
-regra5 = ctrl.Rule(NIF['Sucesso'] & VT['Fracasso'] & RR['Fracasso'] & RSBI['Fracasso'], desmame['Permanece'])
-regra6 = ctrl.Rule(NIF['Fracasso'] & VT['Fracasso'] & RR['Sucesso'] & RSBI['Sucesso'], desmame['Permanece'])
-regra7 = ctrl.Rule(NIF['Fracasso'] & VT['Sucesso'] & RR['Fracasso'] & RSBI['Sucesso'], desmame['Permanece'])
-regra8 = ctrl.Rule(NIF['Sucesso'] & VT['Fracasso'] & RR['Fracasso'] & RSBI['Sucesso'], desmame['Permanece'])
-regra9 = ctrl.Rule(NIF['Sucesso'] & VT['Sucesso'] & RR['Fracasso'] & RSBI['Fracasso'], desmame['Permanece'])
-regra10 = ctrl.Rule(NIF['Sucesso'] & VT['Fracasso'] & RR['Sucesso'] & RSBI['Fracasso'], desmame['Permanece'])
-regra11 = ctrl.Rule(NIF['Fracasso'] & VT['Sucesso'] & RR['Sucesso'] & RSBI['Fracasso'], desmame['Permanece'])
-regra12 = ctrl.Rule(NIF['Fracasso'] & VT['Sucesso'] & RR['Sucesso'] & RSBI['Sucesso'], desmame['Permanece'])
-regra13 = ctrl.Rule(NIF['Sucesso'] & VT['Sucesso'] & RR['Fracasso'] & RSBI['Sucesso'], desmame['Permanece'])
-regra14 = ctrl.Rule(NIF['Sucesso'] & VT['Fracasso'] & RR['Sucesso'] & RSBI['Sucesso'], desmame['Permanece'])
-regra15 = ctrl.Rule(NIF['Sucesso'] & VT['Sucesso'] & RR['Sucesso'] & RSBI['Fracasso'], desmame['Permanece'])
-regra16 = ctrl.Rule(NIF['Sucesso'] & VT['Sucesso'] & RR['Sucesso'] & RSBI['Sucesso'], desmame['Desmamar'])
+regra1 = ctrl.Rule(NIF['Fracasso'] & VT['Fracasso'] & RR['Fracasso'], desmame['Permanece'])
+regra2 = ctrl.Rule(NIF['Fracasso'] & VT['Fracasso'] & RR['Sucesso'], desmame['Permanece'])
+regra3 = ctrl.Rule(NIF['Fracasso'] & VT['Sucesso'] & RR['Fracasso'], desmame['Permanece'])
+regra4 = ctrl.Rule(NIF['Sucesso'] & VT['Fracasso'] & RR['Fracasso'], desmame['Permanece'])
+regra5 = ctrl.Rule(NIF['Sucesso'] & VT['Sucesso'] & RR['Sucesso'], desmame['Desmamar'])
+regra6 = ctrl.Rule(NIF['Fracasso'] & VT['Sucesso'] & RR['Sucesso'], desmame['Permanece'])
+regra7 = ctrl.Rule(NIF['Sucesso'] & VT['Sucesso'] & RR['Fracasso'], desmame['Permanece'])
+regra8 = ctrl.Rule(NIF['Sucesso'] & VT['Fracasso'] & RR['Sucesso'], desmame['Permanece'])
 
 #%%
 """## Sistema de controle"""
 #Criando sistema de controle entre as regras
-sistema_controle = ctrl.ControlSystem([regra1, regra2, regra3, regra4, regra5, 
-                                       regra6, regra7, regra8, regra9, regra10, 
-                                       regra11, regra12, regra13, regra14, regra15, regra16])
+sistema_controle = ctrl.ControlSystem([regra1, regra2, regra3, regra4, regra5, regra6, regra7, regra8])
 sistema = ctrl.ControlSystemSimulation(sistema_controle)
 lista_resultados = []
-def teste_conjunto(NIF, RR, VT, RSBI):
+def teste_conjunto(NIF, RR, VT):
     sistema.input['NIF'] = NIF
     sistema.input['RR'] = RR
     sistema.input['VT'] = VT
-    sistema.input['RSBI'] = RSBI
     sistema.compute()
     print(sistema.output['desmame'])
     desmame.view(sim=sistema)
@@ -133,8 +112,8 @@ def teste_conjunto(NIF, RR, VT, RSBI):
         lista_resultados.append('F')
  
 for i in range(len(df)):
+    teste_conjunto(df['NIF'].iloc[i], df['RR'].iloc[i], df['VT'].iloc[i])
     
-    teste_conjunto(df['NIF'].iloc[i], df['RR'].iloc[i], df['VT'].iloc[i], df['RSBI'].iloc[i])
 #%% Comparando os resulados com o conjunto de referência
 TP = 0
 TN = 0
@@ -153,3 +132,4 @@ sensibilidade = TP/(df[df["Label"] == "S"].count()["Label"])
 especificidade =TN/(df[df["Label"] == "F"].count()["Label"])
 print("SENSIBILIDADE: %s" % sensibilidade)
 print("ESPECIFICIDADE: %s" % especificidade)
+
